7e17d4447eb0450a37149bf0bc5ae604fb04f8d2
Tudor <miutudorgabriel@gmail.com>
2023-03-06 12:35:35 +0200

    Nu cred ca sunt diferente majore;

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
+# Sokoban
+
+
+Sokoban (倉庫番, Sōko-ban, lit.'warehouse keeper') is a puzzle video game in which the player pushes boxes around in a warehouse, trying to get them to storage locations. The game was designed in 1981 by Hiroyuki Imabayashi, and first published in December 1982. [ <sup>[1]</sup> ](https://en.wikipedia.org/wiki/Sokoban)
+
+### Gameplay
+
+The game is played on a board of squares, where each square is a floor or a wall. Some floor squares contain boxes, and some floor squares are marked as storage locations.
+
+The player is confined to the board and may move horizontally or vertically onto empty squares (never through walls or boxes). The player can move a box by walking up to it and push it to the square beyond. Boxes cannot be pulled, and they cannot be pushed to squares with walls or other boxes. The number of boxes equals the number of storage locations. The puzzle is solved when all boxes are placed at storage locations. <sup> [[2]](https://en.wikipedia.org/wiki/Sokoban#Gameplay) </sup>
+
+## Current state of the project:
+
+n/a
+
+---
+
 # OOP Template
 
 ### Tema 0
 
-- [ ] Nume proiect (poate fi schimbat ulterior)
-- [ ] Scurtă descriere a temei alese, ce v-ați propus să implementați
+- [x] Nume proiect (poate fi schimbat ulterior)
+- [x] Scurtă descriere a temei alese, ce v-ați propus să implementați
 
 ## Tema 1
 
 #### Cerințe
-- [ ] definirea a minim 3-4 clase folosind compunere
-- [ ] constructori de inițializare
-- [ ] pentru o clasă: constructor de copiere, `operator=` de copiere, destructor
+- [x] definirea a minim 3-4 clase folosind compunere
+- [x] constructori de inițializare
+- [x] pentru o clasă: constructor de copiere, `operator=` de copiere, destructor
 <!-- - [ ] pentru o altă clasă: constructor de mutare, `operator=` de mutare, destructor -->
 <!-- - [ ] pentru o altă clasă: toate cele 5 funcții membru speciale -->
-- [ ] `operator<<` pentru toate clasele pentru afișare (std::ostream)
+- [x] `operator<<` pentru toate clasele pentru afișare (std::ostream)
 - [ ] cât mai multe `const` (unde este cazul)
 - [ ] implementarea a minim 3 funcții membru publice pentru funcționalități specifice temei alese
 - [ ] scenariu de utilizare a claselor definite:
@@ -54,5 +71,4 @@
 
 ## Resurse
 
-- [SFML](https://github.com/SFML/SFML/tree/aa82ea132b9296a31922772027ad5d14c1fa381b) (Zlib)
 - adăugați trimiteri către resursele externe care v-au ajutat sau pe care le-ați folosit
--- main.cpp
+++ main.cpp
@@ -1,61 +1,111 @@
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <chrono>
-#include <thread>
+#include <queue>
+#include <utility>
 
-#ifdef __linux__
-#include <X11/Xlib.h>
-#endif
+using namespace std;
 
-class SomeClass {
+class Position{
+    int x_axis, y_axis;
+
 public:
-    explicit SomeClass(int) {}
+    Position(int xAxis, int yAxis) : x_axis(xAxis), y_axis(yAxis) {
+        //std::cout << "Constructor initializare poz\n";
+    }
+    Position(const Position& other) : x_axis{other.x_axis}, y_axis{other.y_axis} {
+        //std::cout << "Constructor copiere poz\n";
+    }
+
+    Position& operator=(const Position& other) {
+        x_axis = other.x_axis;
+        y_axis = other.y_axis;
+        //std::cout << "operator= copiere poz\n";
+        return *this;
+    }
+
+    ~Position() {
+        //std::cout << "Destr poz\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Position& st) {
+        os << "x_axis: " << st.x_axis << ", y_axis: " << st.y_axis << "\n";
+        return os;
+    }
+
 };
 
-SomeClass *getC() {
-    return new SomeClass{2};
-}
+class Wall{
+    Position position;
 
-int main() {
-    #ifdef __linux__
-    XInitThreads();
-    #endif
-
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;
-
-    sf::RenderWindow window;
-    // NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:30
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    window.setVerticalSyncEnabled(true);
-    //window.setFramerateLimit(60);
-
-    while(window.isOpen()) {
-        sf::Event e;
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                break;
-            default:
-                break;
-            }
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
-
-        window.clear();
-        window.display();
+public:
+    explicit Wall(const Position &position) : position(position) {
+        //std::cout << "Constructor initializare zid\n";
     }
+
+    friend std::ostream& operator<<(std::ostream& os, const Wall& st) {
+        os << "Wall: " << st.position;
+        return os;
+    }
+
+};
+
+class Player{
+    Position position;
+    std::queue<char> inputs;
+
+public:
+    Player(const Position &position, const queue<char> &inputs) : position(position), inputs(inputs) {
+        //std::cout << "Constructor initializare juc\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Player& st) {
+        os << "Position: " << st.position << "\n";
+        return os;
+    }
+
+};
+
+class Board{
+    int lines, columns;
+    std::vector<Wall> walls;
+    Player player;
+
+public:
+    Board(int lines, int columns, const vector<Wall> &walls, Player player) : lines(lines), columns(columns),
+                                                                              walls(walls), player(std::move(player)) {
+        //std::cout << "Constructor initializare tabla\n";
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Board& st) {
+        os << "lines: " << st.lines << ", columns: " << st.columns << "\nPlayer " << st.player << "walls:" << "\n";
+        for(const auto & wall : st.walls)
+            os << wall;
+
+        return os;
+    }
+
+};
+
+int main(){
+    Position p1{1,2}, p2{5,0};
+    Wall zid1{p2};
+    Wall zid2{p1};
+    std::vector<Wall> vector1;
+    vector1.push_back(zid1);
+    vector1.push_back(zid2);
+
+    std::queue<char> coada;
+    coada.push('a');
+    coada.push('s');
+    coada.push('d');
+    coada.push('w');
+
+    Player jucator{p2,coada};
+    Board tabla{10,10,vector1,jucator};
+
+    std::cout<< "\n";
+    std::cout << p1;
+    std::cout << zid1;
+    std::cout << jucator;
+    std::cout << tabla;
 
     return 0;
 }
