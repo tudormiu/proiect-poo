161a24ad30fceb821cbfd8ed082d689a6e2882aa
Tudor <miutudorgabriel@gmail.com>
2023-03-30 01:03:28 +0300

    adding a title screen and button graphics. Game is now on hold, but a playing sistem will be implemented so levels can be chosen and played

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
     Level level2(file);
 
     sf::RenderWindow window(sf::VideoMode(1440, 810), "My window", sf::Style::Titlebar | sf::Style::Close);
-
+/*
     while (window.isOpen())
     {
         sf::Event event{};
@@ -30,6 +30,33 @@ int main(){
         }
         if(level2.handle_input(window) == 1)
             break;
+    }
+    */
+
+    sf::Sprite sprite;
+    sf::Texture texture;
+    texture.loadFromFile("img/title.jpg");
+    sprite.setTexture(texture);
+    sprite.setPosition(0, 0);
+
+    sf::Sprite button_sprite;
+    sf::Texture button_texture;
+    button_texture.loadFromFile("img/empty_big_button.png");
+    button_sprite.setTexture(button_texture);
+    button_sprite.setPosition(464, 440);
+
+
+    while (window.isOpen()){
+        sf::Event event{};
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(sprite);
+        window.draw(button_sprite);
+        window.display();
+
     }
     return 0;
 }
